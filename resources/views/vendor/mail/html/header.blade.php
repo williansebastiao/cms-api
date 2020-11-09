950d0214e833d87d5ccb828a253e5103e5384d0a
Willians Pereira <willians@4vconnect.com>
2020-11-08 23:11:24 -0300

    feature: template mail

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,38 @@
+<tr>
+<td class="header">
+    <table bgcolor="#f5f5f5" align="center" class="full" border="0" cellpadding="0" cellspacing="0">
+        <tr>
+            <td height="40" style="font-size:0px">&nbsp;</td>
+        </tr>
+        <tr>
+            <td>
+                <table bgcolor="white" width="750" align="center" class="margin-full" border="0" cellpadding="0"
+                       cellspacing="0" style="border-radius: 6px 6px 0 0;">
+                    <tr>
+                        <td>
+                            <table width="600" align="center" class="margin-pad" border="0" cellpadding="0"
+                                   cellspacing="0">
+                                <tr>
+                                    <td height="35" style="font-size:0px">&nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td align="center">
+                                        <a href="#" style="text-decoration: none; display: block; margin: 0 auto;">
+                                            <img width="100"
+                                                 style="max-width: 100px; width: 100%; display: block; line-height: 0px; font-size: 0px; border: 0px; border-radius: 4px;"
+                                                 src="images/logo.png">
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td height="35" style="font-size:0px">&nbsp;</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</td>
+</tr>
