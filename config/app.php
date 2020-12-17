f74baeb3cb9c3889f7b19b502b67fbd7751d749d
Willians Pereira <willians@4vconnect.com>
2020-12-17 19:15:08 -0300

    fix: config url site

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://stup.station.orbital.company'),
+    'url' => env('APP_URL', 'http://stup.station.orbital.company'),
 
     'asset_url' => env('ASSET_URL', null),
 
